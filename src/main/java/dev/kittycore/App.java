054cd1d8e96c55dc77276b4b5962f264d963bcee
eliseydudin <dudinelisey630@gmail.com>
2025-06-16 01:49:27 +0300

    spawn

--- src/main/java/dev/kittycore/App.java
+++ src/main/java/dev/kittycore/App.java
@@ -4,6 +4,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import dev.kittycore.command.Balance;
 import dev.kittycore.command.Pay;
+import dev.kittycore.command.Spawn;
 
 public class App extends JavaPlugin {
     private Economy economy;
@@ -19,6 +20,7 @@ public class App extends JavaPlugin {
         this.getServer().getPluginManager().registerEvents(new KCListener(this), this);
         this.getCommand("balance").setExecutor(new Balance(this.economy));
         this.getCommand("pay").setExecutor(new Pay(this.economy));
+        this.getCommand("pay").setExecutor(new Spawn(this.economy));
     }
 
     public Economy getEconomy() {
---
+++ src/main/java/dev/kittycore/command/Spawn.java
@@ -0,0 +1,47 @@
+package dev.kittycore.command;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import dev.kittycore.Economy;
+import net.md_5.bungee.api.ChatColor;
+
+public class Spawn implements CommandExecutor {
+    private Economy instance;
+
+    public Spawn(Economy in) {
+        this.instance = in;
+    }
+
+    public boolean onCommand(CommandSender sender, Command com, String label, String[] args) {
+        if (!(sender instanceof Player)) {
+            return false;
+        }
+
+        Player player = (Player) sender;
+
+        try {
+            if (this.instance.getBalance(player.getUniqueId()) < 10) {
+                player.sendMessage(ChatColor.AQUA.toString() + ChatColor.BOLD + "[KC] " + ChatColor.RESET
+                        + "insufficient funds :(");
+                return true;
+            }
+            this.instance.give(player.getUniqueId(), -10);
+            Bukkit.getServer().dispatchCommand(Bukkit.getServer().getConsoleSender(),
+                    String.format("mvtp %s world", player.getName()));
+            Bukkit.getServer().dispatchCommand(Bukkit.getServer().getConsoleSender(),
+                    String.format("mvspawn %s", player.getName()));
+            player.sendMessage(ChatColor.AQUA.toString() + ChatColor.BOLD + "[KC] " + ChatColor.RESET
+                    + "teleported ya to spawn!");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+
+        return true;
+    }
+}
